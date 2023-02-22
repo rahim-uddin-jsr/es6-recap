3f1782bfaee62edcea4b1d9bfd615b01deb86246
dev-rahim <rahimuddin5915@gmail.com>
2023-02-22 20:50:50 +0600

    solved another problem of accsing data from an array of objects

--- es-6-intro/es6-recap/access-data-dot-notation-bracket-notation.js
+++ es-6-intro/es6-recap/access-data-dot-notation-bracket-notation.js
@@ -27,3 +27,29 @@ let students = {
 };
 console.log(students[2222].address.city);
 console.log(students[3333].name);
+
+/* 5) How will you display
+a) programming er coddogosti
+b) VW
+c) Bangladesh */
+
+let instructor = {
+  name: "Jhankar Mahbub",
+  entrepreneur: true,
+  additionalData: {
+    writer: true,
+    favoriteHobbies: ["travelling", "Coding"],
+    books: ["programming er bolod to bos", "programming er coddogosti"],
+    moreDetails: {
+      favoriteBasketballTeam: "XYZ",
+      isYoungest: true,
+      hometown: {
+        city: "ABC",
+        state: "VW",
+      },
+      countriesLivedIn: ["Bangladesh", "New York"],
+    },
+  },
+};
+console.log(instructor.additionalData.books[1]);
+console.log(instructor.additionalData.moreDetails.countriesLivedIn[0]);
