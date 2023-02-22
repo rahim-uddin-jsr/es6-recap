5d302abbfe5b8b3b517b74062066f0e23e7d9b34
dev-rahim <rahimuddin5915@gmail.com>
2023-02-22 20:57:06 +0600

    problem solved of accsing data from an array of obkect

--- es-6-intro/es6-recap/access-data-dot-notation-bracket-notation.js
+++ es-6-intro/es6-recap/access-data-dot-notation-bracket-notation.js
@@ -53,3 +53,37 @@ let instructor = {
 };
 console.log(instructor.additionalData.books[1]);
 console.log(instructor.additionalData.moreDetails.countriesLivedIn[0]);
+
+/* How will you display
+a) B
+b) D */
+const studentData = [
+  {
+    class: 10,
+    details: [
+      {
+        studentId: "1",
+        gradingDetails: [{ grade: "A" }],
+      },
+      {
+        studentId: "2",
+        gradingDetails: [{ grade: "B" }],
+      },
+    ],
+  },
+  {
+    class: 11,
+    details: [
+      {
+        studentId: 3,
+        gradingDetails: [{ grade: "B" }],
+      },
+      {
+        studentId: 4,
+        gradingDetails: [{ grade: "D" }],
+      },
+    ],
+  },
+];
+console.log(studentData[1].details[0].gradingDetails[0].grade);
+console.log(studentData[1].details[1].gradingDetails[0].grade);
